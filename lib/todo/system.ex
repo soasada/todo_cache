1884baa4b30cbd3f208c59222c760769daa45f61
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-05-31 17:16:04 +0200

    Create Todo Web HTTP server

--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -1,7 +1,7 @@
 defmodule Todo.System do
   def start_link do
     Supervisor.start_link(
-      [Todo.Metrics, Todo.Registry, Todo.Database, Todo.Cache],
+      [Todo.Metrics, Todo.Registry, Todo.Database, Todo.Cache, Todo.Web],
       strategy: :one_for_one
     )
   end
