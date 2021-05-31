e3aa5509cf8c665e780d5f8d3564087a225626e3
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-05-31 10:23:07 +0200

    TC-1: Database and DatabaseWorker now use poolboy

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -1,39 +1,34 @@
 defmodule Todo.Database do
-  @pool_size 3
   @db_folder "./persist"
 
-  def start_link do
-    children = Enum.map(1..@pool_size, &worker_spec/1)
-    Supervisor.start_link(children, strategy: :one_for_one)
-  end
-
   def store(key, data) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.store(key, data)
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_pid ->
+        Todo.DatabaseWorker.store(worker_pid, key, data)
+      end
+    )
   end
 
   def get(key) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.get(key)
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_pid ->
+        Todo.DatabaseWorker.get(worker_pid, key)
+      end
+    )
   end
 
   def child_spec(_) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, []},
-      type: :supervisor
-    }
-  end
-
-  defp choose_worker(key) do
-    :erlang.phash2(key, @pool_size) + 1
-  end
-
-  defp worker_spec(worker_id) do
-    default_worker_spec = {Todo.DatabaseWorker, {@db_folder, worker_id}}
-    Supervisor.child_spec(default_worker_spec, id: worker_id)
+    :poolboy.child_spec(
+      __MODULE__,
+      [
+        name: {:local, __MODULE__},
+        worker_module: Todo.DatabaseWorker,
+        size: 3
+      ],
+      [@db_folder]
+    )
   end
 end
 
@@ -42,17 +37,17 @@ end
 defmodule Todo.DatabaseWorker do
   use GenServer
 
-  def start_link({folder, worker_id}) do
-    IO.puts("Starting to-do database worker #{worker_id}.")
-    GenServer.start_link(__MODULE__, folder, name: via_tuple(worker_id))
+  def start_link(folder) do
+    IO.puts("Starting to-do database worker.")
+    GenServer.start_link(__MODULE__, folder)
   end
 
   def store(worker_id, key, data) do
-    GenServer.cast(via_tuple(worker_id), {:store, key, data})
+    GenServer.cast(worker_id, {:store, key, data})
   end
 
   def get(worker_id, key) do
-    GenServer.call(via_tuple(worker_id), {:get, key})
+    GenServer.call(worker_id, {:get, key})
   end
 
   @impl true
@@ -82,9 +77,5 @@ defmodule Todo.DatabaseWorker do
 
   defp file_name(folder, key) do
     Path.join(folder, to_string(key))
-  end
-
-  defp via_tuple(worker_id) do
-    Todo.Registry.via_tuple({__MODULE__, worker_id})
   end
 end
