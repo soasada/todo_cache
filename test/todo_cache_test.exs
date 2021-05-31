a792e91888178cd34658fdec6e8d23d70f3a3216
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-05-31 10:50:08 +0200

    TC-1: Fix tests, now we don't need to pass cache pid to cache server process

--- test/todo_cache_test.exs
+++ test/todo_cache_test.exs
@@ -3,15 +3,15 @@ defmodule TodoCacheTest do
 
   test "server_process should return to-do server pid" do
     # {:ok, cache} = Todo.Cache.start() ----- not needed because we have converted the system into an OTP Application
-    bob_pid = Todo.Cache.server_process(cache, "bob")
+    bob_pid = Todo.Cache.server_process("bob")
 
-    assert bob_pid != Todo.Cache.server_process(cache, "alice")
-    assert bob_pid == Todo.Cache.server_process(cache, "bob")
+    assert bob_pid != Todo.Cache.server_process("alice")
+    assert bob_pid == Todo.Cache.server_process("bob")
   end
 
   test "to-do operations" do
     # {:ok, cache} = Todo.Cache.start()
-    alice_pid = Todo.Cache.server_process(cache, "alice")
+    alice_pid = Todo.Cache.server_process("alice")
     Todo.Server.add(alice_pid, ~D[2018-12-19], "Dentist")
     entries = Todo.Server.entries(alice_pid, ~D[2018-12-19])
 
